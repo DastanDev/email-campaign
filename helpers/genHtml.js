362003e3ef2a9157ca59b9c5defbe6b051a661a8
DastanDev <dstnmhmd@gmail.com>
2021-09-07 19:14:20 +0300

    encryption

--- helpers/genHtml.js
+++ helpers/genHtml.js
@@ -1,29 +1,24 @@
 const fs = require('fs')
-const CryptoJS = require('crypto-js')
 const cheerio = require('cheerio')
-
-const encrypt = text => {
-  return CryptoJS.enc.Base64.stringify(CryptoJS.enc.Utf8.parse(text))
-}
+const he = require('he')
+var ent = require('ent')
 
 const genHtml = () => {
   fs.readFile('./letter.html', (err, data) => {
     if (err) return console.log(err.message)
     //
     const $ = cheerio.load(data.toString())
-    const toBeEncrypted = $('#encrypt')[0].children[0].data
-    const encrypted = encrypt(toBeEncrypted)
-    $('#encrypt')[0].children[0].data = encrypted
-    const html = $.html()
-    // write file
-    fs.writeFile('files/letter.html', html, async err => {
-      if (err) throw new Error(err)
-    })
+    const toBeEncrypted = $('.encrypt')
+    for (let i = 0; i < toBeEncrypted.length; i++) {
+      const text = toBeEncrypted[i].children[0].data
+      const encrypted = he.encode(text, { encodeEverything: true })
+      $('.encrypt')[i].children[0].data = encrypted
+    }
+    const encryptedHtmlWithAmp = $.html()
+    const encryptedWithoutAmp = encryptedHtmlWithAmp.replace(/&amp;/g, '&')
 
-    // document.getElementById('encrypted').innerText = decrypt(encFromDom)
+    fs.writeFileSync('files/letter.html', encryptedWithoutAmp)
   })
 }
-
-genHtml()
 
 module.exports = genHtml
